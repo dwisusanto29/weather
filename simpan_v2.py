25cca09ecf4dfe11301704a04da8bf07a69a55d3
dwisusanto29 <dw.susanto@gmail.com>
2017-01-24 02:58:26 +0700

    Build on 2017-01-24 at  2:58:26 am WIB

--- simpan_v2.py
+++ simpan_v2.py
@@ -8,5 +8,5 @@ dateparse = lambda dates: pd.datetime.strptime(dates,'%Y-%m-%d %H:%M:%S').strfti
 data = pd.read_csv(filename, sep=",",  skiprows=1, names=['waktu', 'tanggal', 'derajat_arah_angin', 'kecepatan_angin_avg', 'kecepatan_angin_max', 'hujan_jam','hujan_hari','suhu', 'kelembaban','tekanan_udara'], date_parser=dateparse, index_col=0) 
 #dataku = data.asfreq('2Min', method='pad')
 
-engine = create_engine('mysql+pymysql://alvin:alvin@202.124.205.201:3306/db_pengamatan', echo=False)
+engine = create_engine('mysql+pymysql://alvin:alvin@202.124.205.201:3306/db_dws', echo=False)
 data.to_sql(name='tb_pengamatan', con=engine, if_exists = 'append', index=True)
