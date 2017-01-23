8bd59bfa13c3f45dfd1f3a5f578f5ef02079c001
dwisusanto29 <dw.susanto@gmail.com>
2017-01-23 08:00:02 +0700

    Build on 2017-01-23 at  8:00:02 am WIB

---
+++ clean2.py
@@ -0,0 +1,19 @@
+
+import pandas as pd
+import sys
+from datetime import datetime, timedelta
+
+d = datetime.today() - timedelta(days=1)
+name = d.strftime("%Y%m%d")
+
+filename = "/home/pi/DataArduino/data/data"+name+".csv"
+fileout = "/home/pi/DataArduino/weather/a"+name+".csv"
+data = pd.read_csv(filename, sep=",",  skiprows=1, names=['waktu', 'tanggal', 'winddir', 'windspeed1', 'windspeed2', 'Rain1','Rain2','temperature', 'humidity','Pressure'], low_memory=False) 
+
+baru = data.dropna(axis=0)
+dataku = baru.drop_duplicates(['waktu'])
+
+dataku.to_csv(fileout, sep=',', encoding='utf-8', index=False, header=False)
+
+
+
---
+++ code/simpan_v2_h-1.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import sys
+from sqlalchemy import create_engine
+from datetime import datetime, timedelta
+
+d = datetime.today() - timedelta(days=1)
+name = d.strftime("%Y%m%d")
+
+#filename = "/home/pi/DataArduino/data/data"+name+".csv"
+filename = "/home/pi/DataArduino/weather/a"+name+".csv"
+#filename = sys.argv[1]
+#fileout = filename[3:]
+dateparse = lambda dates: pd.datetime.strptime(dates,'%Y-%m-%d %H:%M:%S').strftime('%Y-%m-%d %H:%M:%S')
+data = pd.read_csv(filename, sep=",",  skiprows=1, names=['waktu', 'tanggal', 'derajat_arah_angin', 'kecepatan_angin_avg', 'kecepatan_angin_max', 'hujan_jam','hujan_hari','suhu', 'kelembaban','tekanan_udara'], date_parser=dateparse, index_col=0) 
+#dataku = data.asfreq('2Min', method='pad')
+
+engine = create_engine('mysql+pymysql://alvin:alvin@202.124.205.201:3306/db_dws', echo=False)
+data.to_sql(name='tb_pengamatan', con=engine, if_exists = 'append', index=True)
