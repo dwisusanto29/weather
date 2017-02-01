81e3f8cfd988ec2a6b30b08bc9df5b26dc8303e2
dwisusanto29 <dw.susanto@gmail.com>
2017-02-01 20:39:53 +0700

    Build on 2017-02-01 at 08:39:53 PM

--- code/clean_v2.py
+++ code/clean_v2.py
@@ -13,8 +13,8 @@ fileout = "/home/pi/DataArduino/weather/data/a"+name+".csv"
 
 data = pd.read_csv(filename, sep=",",  skiprows=1, names=['waktu', 'tanggal', 'winddir', 'windspeed1', 'windspeed2', 'Rain1','Rain2','temperature', 'humidity','Pressure'], low_memory=False) 
 
-#satu = data[data.humidity > 0]
-baru = data.dropna(axis=0)
+satu = data[data.humidity > 0]
+baru = satu.dropna(axis=0)
 dataku = baru.drop_duplicates(['waktu'])
 dataku.to_csv(fileout, sep=',', encoding='utf-8', index=False, header=False)
 
